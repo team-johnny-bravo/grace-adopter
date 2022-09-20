5535c93308909a55017f13af6f2c1463674e7ea3
parzidog <kenneth.schaefer0@gmail.com>
2022-09-20 15:43:16 -0500

    Create add pet page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,8 +6,8 @@ import { useNavigate } from 'react-router-dom';
 const NavBar = () => {
     const navigate =useNavigate()
     const user={
-        name: "",
-        isAdmin: false
+        name: "Jeff",
+        isAdmin: true
     };
 
 
@@ -18,7 +18,7 @@ const NavBar = () => {
             <a href="/home">Home</a>
             <a href="/about">About</a>
             <a href="/contact">Contact</a>
-            {user.isAdmin ? <button>Add Pet</button> : null}
+            {user.isAdmin ? <button onClick={()=>navigate('/addpet')}>Add Pet</button> : null}
             {user.name ? <button>Welcome, {user.name}</button> : <button onClick={()=>navigate('/login')}>Login / Signup</button>}
         </h2>
     </div>
