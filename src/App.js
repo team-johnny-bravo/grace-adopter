62b309e2b819b090687b451e72907648bebb304c
parzidog <kenneth.schaefer0@gmail.com>
2022-09-20 11:49:37 -0500

    Create single pet page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { useEffect } from "react";
 // import { useDispatch } from "react-redux";
 // import { Nav, Home } from "./components";
-import Home from './components/Home'
+import Home from './components/Home.jsx'
+import SinglePetPage from "./components/SinglePetPage.jsx";
 // import { getCampuses } from "./store/reducers/campusReducer";
 // import { getStudents } from "./store/reducers/studentReducer";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  // const dispatch = useDispatch();
-
-  //Load campuses and students on app load
-  // useEffect(() => {
-  //   dispatch(getCampuses());
-  //   dispatch(getStudents());
-  // }, []);
+ 
 
   return (
     <>
       {/* <Nav /> */}
       <Routes>
-        <Route index element={<Home />} />
+        <Route index element={<SinglePetPage />} />
         {/* <Route path="campuses" element={<Campuses />} />
         <Route path="students" element={<Students />} />
         <Route path="campuses/:campusId" element={<CampusPage />} />
---
+++ src/components/SinglePetPage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const SinglePetPage = () => {
+  const pet ={
+  "id": 10,
+  "name": "Emmeline",
+  "age": 3,
+  "description": 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla rem minima impedit dolorum id pariatur, qui consequatur doloremque commodi animi! Ipsam quaerat tempore accusantium blanditiis odit obcaecati esse similique velit.',
+  "favoriteToys":['rope', 'tennis ball', 'frisbee'],
+  "imgUrl": "http://dummyimage.com/218x100.png/dddddd/000000"
+}
+  return (
+    <div>
+      <div>
+        <img src={pet.imgUrl} alt={pet.name} />
+      </div>
+      <div>
+        <div>
+          <h1>{pet.name}</h1>
+          <p className='singlePetAge'>Age: {pet.age}</p>
+          <h3>Description: </h3><br/>
+          <p>{pet.description}</p>
+          <h3>Favorite Toys:</h3> 
+          <p>{pet.favoriteToys.join(', ')}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SinglePetPage
