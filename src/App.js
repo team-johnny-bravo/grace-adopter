90e3fa17887576533ba857e055c5d06a50858deb
Corum Gold <goldcorum@gmail.com>
2022-09-20 10:48:27 -0400

    Copy files from JPFP

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Nav } from "./components";
+import { getCampuses } from "./store/reducers/campusReducer";
+import { getStudents } from "./store/reducers/studentReducer";
+import { Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  const dispatch = useDispatch();
+
+  //Load campuses and students on app load
+  useEffect(() => {
+    dispatch(getCampuses());
+    dispatch(getStudents());
+  }, []);
+
+  return (
+    <>
+      <Nav />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="campuses" element={<Campuses />} />
+        <Route path="students" element={<Students />} />
+        <Route path="campuses/:campusId" element={<CampusPage />} />
+        <Route path="students/:studentId" element={<StudentPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
