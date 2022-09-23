59ad9293c8831638efbe4ef88baee5bdd42200c0
aaronub <wo-08@hotmail.com>
2022-09-23 15:52:38 -0400

    add favorite toys to seed.js

--- src/components/users/Signup.jsx
+++ src/components/users/Signup.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+
+
 const Signup = () => {
     // const dispatch = useDispatch();
     const navigate = useNavigate();
