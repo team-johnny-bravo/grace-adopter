dc76b637439db7a4ec16c105b437cbe851b538ba
parzidog <kenneth.schaefer0@gmail.com>
2022-09-28 08:45:19 -0500

    change db/server files

--- server/db/database.js
+++ server/db/database.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/postgresql-pointy-12358' || process.env.DATABASE_URL);
+console.log(process.env.DATABASE_URL)
+const db = new Sequelize('postgres://localhost:5432/grace-adopter' || process.env.DATABASE_URL);
 
 module.exports = db;
 
