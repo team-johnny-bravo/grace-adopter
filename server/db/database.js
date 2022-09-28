c5392dc5f71282fcea859be448c82ee944cd9698
parzidog <kenneth.schaefer0@gmail.com>
2022-09-28 08:40:34 -0500

    change db/server files

--- server/db/database.js
+++ server/db/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/postgresql-pointy-12358', { logging: false });
+const db = new Sequelize('postgres://localhost:5432/postgresql-pointy-12358' || process.env.DATABASE_URL);
 
 module.exports = db;
 
